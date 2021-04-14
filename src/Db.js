d9970d0ee9727ff666966a2049a19645cb25902a
Anderson <andersonecomp091@gmail.com>
2021-04-14 09:29:57 -0300

    removing duplicated code

--- src/Db.js
+++ src/Db.js
@@ -33,19 +33,6 @@ class DB {
 
  test(){
   console.log('test');
-  console.log('test');
-  console.log('test');
-  console.log('test');
-  console.log('test');
-  console.log('test');
-  console.log('test');
-  console.log('test');
-  console.log('test');
-  console.log('test');
-  console.log('test');
-  console.log('test');
-  console.log('test');
-  console.log('test');
  }
 
   async insert({ collectionName, data }) {
