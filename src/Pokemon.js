8ae85b253da311346ad005790b05c127c62514b6
Anderson <andersonecomp091@gmail.com>
2021-04-15 07:24:32 -0300

    removing an issue introduced

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -12,27 +12,7 @@ class Pokemon extends BaseCrud {
     this.weight = weight;
     this.skills = skills;
   }
-  test(){
-    console.log('test');
-    console.log('test');
-    console.log('test');
-    console.log('test');
-    console.log('test');
-    console.log('test');
-    console.log('test');
-    console.log('test');
-    console.log('test');
-    console.log('test');
-    console.log('test');
-    console.log('test');
-    console.log('test');
-    console.log('test');
-    console.log('test');
-    console.log('test');
-    console.log('test');
-    console.log('test');
-    console.log('test');
-  }
+
   getProperties(){
     const name = this.name;
     const types = this.types;
